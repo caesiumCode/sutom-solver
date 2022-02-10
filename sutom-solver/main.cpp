c2846233f30cf4d189eb079b60ffcf9987aefa1d
caesiumCode <caesiumcode@aol.com>
2022-02-10 11:24:14 +0300

    Initial Commit

---
+++ sutom-solver/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  sutom-solver
+//
+//  Created by Stephen Jaud on 10/02/2022.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
